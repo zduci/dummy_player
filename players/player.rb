ba844909d9bd0dffd62a9030cab4dee8b45658d7
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-11 19:35:54 +0000

    Basic guesssing

--- players/player.rb
+++ players/player.rb
@@ -1,15 +1,25 @@
 #require 'my_helper'
 class Player
+  COMMON = ["e", "t", "a", "o", "i"]
+
   def name
     "Radu"
   end
 
   def take_turn(state, guesses)
+    turn = which_turn(guesses)
+    if turn < COMMON.size + 1
+      COMMON[turn - 1]
+    else
+      look_up(state, guesses)
+    end
   end
 
   def which_turn(guesses)
     guesses.size + 1
   end
 
-  
+  def look_up(state, guesses)
+  end
 end
+
