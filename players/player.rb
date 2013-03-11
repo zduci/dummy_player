3e9dac46a54dba590d17af18d7b155ffe6b18777
Radu Busuioc <ra.busuioc@gmail.com>
2013-03-11 19:20:00 +0000

    Add which_turn

--- players/player.rb
+++ players/player.rb
@@ -1,10 +1,14 @@
-require 'my_helper'
+#require 'my_helper'
 class Player
   def name
     "Radu"
   end
 
   def take_turn(state, guesses)
-    MyHelper.answer
+    if which_turn(guesses) == 
+  end
+
+  def which_turn(guesses)
+    guesses.size + 1
   end
 end
